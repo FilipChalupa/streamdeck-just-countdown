602674ec2d1e45a6f35a793f3d34fc3073b400eb
Filip Chalupa <chalupa.filip@gmail.com>
2023-09-29 15:44:45 +0200

    Get time

--- src/eu.just-countdown.sdPlugin/app.js
+++ src/eu.just-countdown.sdPlugin/app.js
@@ -21,7 +21,7 @@ const timeSettingsToSeconds = (hours, minutes, seconds) => {
 }
 
 const setAction = new Action('eu.just-countdown.set-action')
-setAction.onKeyUp(async ({ payload }) => {
+setAction.onKeyUp(async ({ payload, context }) => {
 	const { identifier } = payload.settings
 	if (!identifier) {
 		return
@@ -36,10 +36,11 @@ setAction.onKeyUp(async ({ payload }) => {
 			identifier,
 		)}&set=${seconds}`,
 	)
+	$SD.showOk(context)
 })
 
 const viewAction = new Action('eu.just-countdown.view-action')
-viewAction.onKeyUp(async ({ payload }) => {
+viewAction.onKeyUp(async ({ payload, context }) => {
 	const { identifier } = payload.settings
 	if (!identifier) {
 		return
@@ -49,13 +50,52 @@ viewAction.onKeyUp(async ({ payload }) => {
 			identifier,
 		)}&togglePaused`,
 	)
+	$SD.showOk(context)
 })
-viewAction.onDialRotate(({ action, context, device, event, payload }) => {
-	console.log('Your dial code goes here!')
+const countdownIframes = document.querySelector('#countdowns')
+window.addEventListener('message', (event) => {
+	if (
+		event.origin === 'https://just-countdown.eu' &&
+		typeof event.data.id === 'string'
+	) {
+		const iframe = countdownIframes.querySelector(
+			`[src="https://just-countdown.eu/screen?id=${encodeURIComponent(
+				event.data.id,
+			)}"]`,
+		)
+		if (!iframe) {
+			return
+		}
+		const context = iframe.getAttribute('data-context')
+		$SD.setTitle(context, event.data.formattedTime)
+	}
 })
+const handleViewSettingsAppearAction = async ({ payload, context }) => {
+	let iframe = countdownIframes.querySelector(`[data-context="${context}"]`)
+	const { identifier } = payload.settings
+	if (!identifier) {
+		if (iframe) {
+			iframe.remove()
+		}
+		return
+	}
+	if (!iframe) {
+		iframe = document.createElement('iframe')
+		iframe.setAttribute('data-context', context)
+		countdownIframes.appendChild(iframe)
+	}
+	const src = `https://just-countdown.eu/screen?id=${encodeURIComponent(
+		identifier,
+	)}`
+	if (src !== iframe.src) {
+		iframe.src = src
+	}
+}
+viewAction.onDidReceiveSettings(handleViewSettingsAppearAction)
+viewAction.onWillAppear(handleViewSettingsAppearAction)
 
 const adjustAction = new Action('eu.just-countdown.adjust-action')
-adjustAction.onKeyUp(async ({ payload }) => {
+adjustAction.onKeyUp(async ({ payload, context }) => {
 	const { identifier, mode } = payload.settings
 	if (!identifier) {
 		return
@@ -73,4 +113,5 @@ adjustAction.onKeyUp(async ({ payload }) => {
 			identifier,
 		)}&adjust=${seconds}`,
 	)
+	$SD.showOk(context)
 })
