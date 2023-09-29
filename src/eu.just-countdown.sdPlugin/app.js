2b19735ceaf4adeca72dc98d6c39e74eacdd6555
Filip Chalupa <chalupa.filip@gmail.com>
2023-09-29 14:23:07 +0200

    Add set and adjust action

--- src/eu.just-countdown.sdPlugin/app.js
+++ src/eu.just-countdown.sdPlugin/app.js
@@ -10,21 +10,60 @@ $SD.onConnected(
 	},
 )
 
-const myAction = new Action('com.elgato.template.action')
-
-myAction.onKeyUp(({ action, context, device, event, payload }) => {
-	console.log('Your key code goes here!')
-	console.log({ action, context, device, event, payload })
-})
-myAction.onDialRotate(({ action, context, device, event, payload }) => {
-	console.log('Your dial code goes here!')
-})
+const timeSettingsToSeconds = (hours, minutes, seconds) => {
+	const parsePart = (part) => {
+		const parsed = parseInt(part, 10)
+		return isNaN(parsed) ? 0 : parsed
+	}
+	return (
+		parsePart(hours) * 60 * 60 + parsePart(minutes) * 60 + parsePart(seconds)
+	)
+}
 
 const setAction = new Action('eu.just-countdown.set-action')
-setAction.onKeyUp(({ action, context, device, event, payload }) => {
+setAction.onKeyUp(async ({ payload }) => {
+	const { identifier } = payload.settings
+	if (!identifier) {
+		return
+	}
+	const seconds = timeSettingsToSeconds(
+		payload.settings.hours,
+		payload.settings.minutes,
+		payload.settings.seconds,
+	)
+	await fetch(
+		`https://just-countdown.eu/api/control?id=${encodeURIComponent(
+			identifier,
+		)}&set=${seconds}`,
+	)
+})
+
+const viewAction = new Action('eu.just-countdown.view-action')
+viewAction.onKeyUp(({ action, context, device, event, payload }) => {
 	console.log('Your key code goes here!')
 	console.log({ action, context, device, event, payload })
 })
-setAction.onDialRotate(({ action, context, device, event, payload }) => {
+viewAction.onDialRotate(({ action, context, device, event, payload }) => {
 	console.log('Your dial code goes here!')
+})
+
+const adjustAction = new Action('eu.just-countdown.adjust-action')
+adjustAction.onKeyUp(async ({ payload }) => {
+	const { identifier, mode } = payload.settings
+	if (!identifier) {
+		return
+	}
+	const sign = mode === 'add' ? 1 : -1
+	const seconds =
+		sign *
+		timeSettingsToSeconds(
+			payload.settings.hours,
+			payload.settings.minutes,
+			payload.settings.seconds,
+		)
+	await fetch(
+		`https://just-countdown.eu/api/control?id=${encodeURIComponent(
+			identifier,
+		)}&adjust=${seconds}`,
+	)
 })
